a651f92d0c41f83c269ad678842055f8f620dfcd
danilovict2 <danilosindric@gmail.com>
2024-11-02 19:54:41 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -28,43 +28,44 @@ func main() {
 			os.Exit(1)
 		}
 
-		go func() {
-			defer conn.Close()
-			for {
-				resp := NewResp(conn)
-				value, err := resp.Read()
+		go handleConnection(conn)
+	}
+}
 
-				if errors.Is(err, io.EOF) {
-					fmt.Println("Client closed the connections:", conn.RemoteAddr())
-					break
-				} else if err != nil {
-					fmt.Println("Error while reading the message:", err)
-					break
-				}
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
 
-				if value.typ != "array" {
-					fmt.Println("Invalid request, expected array")
-					break
-				}
-			
-				if len(value.array) == 0 {
-					fmt.Println("Invalid request, expected array length > 0")
-					break
-				}
+		if errors.Is(err, io.EOF) {
+			fmt.Println("Client closed the connections:", conn.RemoteAddr())
+			break
+		} else if err != nil {
+			fmt.Println("Error while reading the message:", err)
+			break
+		}
 
-				command := strings.ToUpper(value.array[0].bulk)
-				handler, ok := Handlers[command]
-				if !ok {
-					fmt.Println("Invalid command: ", command)
-					break
-				}
-				
-				writer := NewWriter(conn)
-				err = writer.Write(handler(value.array[1:]))
-				if err != nil {
-					fmt.Println("Error while writing the message:", err)
-				}
-			}
-		}()
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			break
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			break
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			break
+		}
+
+		writer := NewWriter(conn)
+		if err = writer.Write(handler(value.array[1:])); err != nil {
+			fmt.Println("Error while writing the message:", err)
+		}
 	}
 }
