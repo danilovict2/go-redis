dd41923c3b9ffdb1b2e59b26d11ccff27cfb474b
danilovict2 <danilosindric@gmail.com>
2024-11-11 20:31:26 +0100

    implement part 3 of the handshake that happens when a replica connects to master

--- app/server.go
+++ app/server.go
@@ -77,6 +77,8 @@ func sendHandshake(replicaof string, port string) {
 	conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n" + port + "\r\n"))
 	time.Sleep(1 * time.Second)
 	conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+	time.Sleep(1 * time.Second)
+	conn.Write([]byte("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"))
 }
 
 func handleConnection(conn net.Conn) {
