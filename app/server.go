9ed9bc891edad0d6ad74e5fab6604a3894ef05b9
danilovict2 <danilosindric@gmail.com>
2024-11-11 20:57:10 +0100

    add support for sending an empty RDB file to the replica

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"errors"
 	"flag"
 	"fmt"
@@ -115,6 +116,11 @@ func handleConnection(conn net.Conn) {
 		writer := NewWriter(conn)
 		if err = writer.Write(handler(value.Array[1:])); err != nil {
 			fmt.Println("Error while writing the message:", err)
+		}
+
+		if command == "PSYNC" {
+			emptyRDB, _ := hex.DecodeString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
+			conn.Write(append([]byte(fmt.Sprintf("$%d\r\n", len(emptyRDB))), emptyRDB...))
 		}
 	}
 }
