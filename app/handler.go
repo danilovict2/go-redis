11354e71a5b5671a11e6aecbf877499a29dcaefc
danilovict2 <danilosindric@gmail.com>
2024-11-22 20:52:54 +0100

    extend support to XREAD to allow querying multiple streams

--- app/handler.go
+++ app/handler.go
@@ -215,6 +215,7 @@ func typ(args []resp.Value) resp.Value {
 }
 
 type Stream struct {
+	key     string
 	last    string
 	entries []StreamEntry
 }
@@ -239,6 +240,7 @@ func xadd(args []resp.Value) resp.Value {
 
 	if !ok {
 		stream = Stream{
+			key:     streamKey,
 			last:    "0-0",
 			entries: make([]StreamEntry, 0),
 		}
@@ -376,44 +378,55 @@ func xrangeFormatArgs(arg string, stream Stream) (string, int64) {
 }
 
 func xread(args []resp.Value) resp.Value {
-	if len(args) != 3 {
-		return resp.Value{Typ: "error", Str: "ERR wrong number of arguments for 'xread' command"}
+	streams := make([]Stream, 0)
+	for _, arg := range args[1:] {
+		STREAMsMu.RLock()
+		stream, ok := STREAMs[arg.Bulk]
+		STREAMsMu.RUnlock()
+
+		if !ok {
+			break
+		}
+
+		streams = append(streams, stream)
 	}
 
-	streamKey := args[1].Bulk
-	STREAMsMu.RLock()
-	stream, ok := STREAMs[streamKey]
-	STREAMsMu.RUnlock()
-
 	ret := resp.Value{Typ: "array"}
-	if !ok {
+	if len(streams) == 0 {
 		return ret
 	}
 
-	startVal, startSeq := xrangeFormatArgs(args[2].Bulk, stream)
-	respStream := resp.Value{Typ: "array"}
-	respStream.Array = append(respStream.Array, resp.Value{Typ: "bulk", Bulk: streamKey})
-
-	for _, entry := range stream.entries {
-		entryId := strings.Split(entry.id, "-")
-		seq, _ := strconv.ParseInt(entryId[1], 10, 64)
-
-		if (entryId[0] > startVal || (entryId[0] == startVal && seq > startSeq)) {
-			respEntries := resp.Value{Typ: "array"}
-			respEntry := resp.Value{Typ: "array"}
-			respEntry.Array = append(respEntry.Array, resp.Value{Typ: "bulk", Bulk: entry.id}) 
-
-			respKVPs := resp.Value{Typ: "array"}
-			for key, value := range entry.KVPs {
-				respKVPs.Array = append(respKVPs.Array, resp.Value{Typ: "bulk", Bulk: key}, resp.Value{Typ: "bulk", Bulk: value})
-			}
-
-			respEntry.Array = append(respEntry.Array, respKVPs)
-			respEntries.Array = append(respEntries.Array, respEntry)
-			respStream.Array = append(respStream.Array, respEntries)
+	for i, stream := range streams {
+		if len(streams)+1+i > len(args) {
+			return resp.Value{Typ: "error", Str: "ERR Unbalanced 'xread' list of streams: for each stream key an ID or '$' must be specified."}
 		}
+
+		startVal, startSeq := xrangeFormatArgs(args[len(streams)+1+i].Bulk, stream)
+		respStream := resp.Value{Typ: "array"}
+		respStream.Array = append(respStream.Array, resp.Value{Typ: "bulk", Bulk: stream.key})
+
+		for _, entry := range stream.entries {
+			entryId := strings.Split(entry.id, "-")
+			seq, _ := strconv.ParseInt(entryId[1], 10, 64)
+
+			if entryId[0] > startVal || (entryId[0] == startVal && seq > startSeq) {
+				respEntries := resp.Value{Typ: "array"}
+				respEntry := resp.Value{Typ: "array"}
+				respEntry.Array = append(respEntry.Array, resp.Value{Typ: "bulk", Bulk: entry.id})
+
+				respKVPs := resp.Value{Typ: "array"}
+				for key, value := range entry.KVPs {
+					respKVPs.Array = append(respKVPs.Array, resp.Value{Typ: "bulk", Bulk: key}, resp.Value{Typ: "bulk", Bulk: value})
+				}
+
+				respEntry.Array = append(respEntry.Array, respKVPs)
+				respEntries.Array = append(respEntries.Array, respEntry)
+				respStream.Array = append(respStream.Array, respEntries)
+			}
+		}
+
+		ret.Array = append(ret.Array, respStream)
 	}
 
-	ret.Array = append(ret.Array, respStream)
 	return ret
 }
